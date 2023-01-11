75339a0d3918804f453687497335f0af683a5a36
Jason Christopher <jchristopher2448@gmail.com>
2023-01-11 06:24:26 -0600

    set up lab

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LAB - Class 19
+# LAB - Class 18
 
 ## Project: Caesar Cipher
 
---
+++ caesar_cipher/cipher.py
@@ -0,0 +1,10 @@
+def encrypt(string, shift):
+    pass
+
+
+def decrypt(string, shift):
+    pass
+
+
+def crack(string):
+    pass
