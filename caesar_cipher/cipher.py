5276ba870c7dd6cd42c69194bd6f7bd77e4550c1
Jason Christopher <jchristopher2448@gmail.com>
2023-01-11 06:48:20 -0600

    encrypt tests passing

--- caesar_cipher/cipher.py
+++ caesar_cipher/cipher.py
@@ -1,10 +1,23 @@
-def encrypt(string, shift):
-    pass
+def encrypt(string, shift_input):
+    shift = shift_input % 26
+    result = ""
+    for char in string:
+        if char.isupper():
+            new_num = ord(char) + shift
+            result += chr(new_num - 26) if new_num > 90 else chr(new_num)
+        elif char.islower():
+            new_num = ord(char) + shift
+            result += chr(new_num - 26) if new_num > 122 else chr(new_num)
+        else:
+            result += char
+    return result
 
 
-def decrypt(string, shift):
-    pass
+def decrypt(string, shift_input):
+    shift = shift_input % 26
 
 
 def crack(string):
     pass
+
+
